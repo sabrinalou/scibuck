0634d5e1eea33c35e6b2b9fca0598b7aa263affe
sabrinalou3 <87052249+sabrinalou3@users.noreply.github.com>
2022-09-24 19:30:46 -0700

    adding input text box (not done!)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = event => {
+    // 👇️ toggle shown state
+    setIsShown(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <form>
+          <label for="fname">Paste paragraphs here: </label>
+
+            <input type="text"></input>
+            <br></br>
+            <button onClick={handleClick} type="submit">Convert</button>
+          </form>
+
+          {/* show elements on click */}
+{/*}            {isShown && (
+              <div>
+                <p>content shown here</p>
+              </div>
+)} */}
+
+            {/* show component on click */}
+            {isShown && <gistOutput />}
+            
+          </div>
       </header>
     </div>
   );
+}
+
+function gistOutput() {
+  return (
+    <div>
+      <p>Content</p>
+    </div>
+  )
 }
 
 export default App;
